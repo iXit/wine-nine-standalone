f33c6e6b8790be907cec782dbf0e231f69739377
Andre Heider <a.heider@gmail.com>
2018-11-08 19:43:45 +0100

    add release.sh to create release tarballs

---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+SRC=`dirname $(readlink -f $0)`
+TMP=`mktemp -d`
+
+meson \
+	--cross-file "$SRC/tools/cross-wine64" \
+	--buildtype "release" \
+	--prefix "$TMP/nine" \
+	--bindir bin64 \
+	--libdir lib64 \
+	"$TMP/build64"
+
+ninja -C "$TMP/build64" install
+
+meson \
+	--cross-file "$SRC/tools/cross-wine32" \
+	--buildtype "release" \
+	--prefix "$TMP/nine" \
+	--bindir bin32 \
+	--libdir lib32 \
+	"$TMP/build32"
+
+ninja -C "$TMP/build32" install
+
+install -m 755 "$SRC/tools/nine-install.sh" "$TMP/nine/"
+tar -C "$TMP" -czf "$TMP/nine.tar.gz" nine
+
+echo "\nenjoy your release: $TMP/nine.tar.gz\n"
+
+exit 0
