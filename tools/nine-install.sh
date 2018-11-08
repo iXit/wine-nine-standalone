606dc29eb1b18a35a3a006a64285fa8aafbd2910
Andre Heider <a.heider@gmail.com>
2018-11-08 20:21:21 +0100

    tools: add nine-install.sh to setup a WINE prefix
    This is tailored to the upcoming release tarball, and will be used by
    it.

---
+++ tools/nine-install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -e
+
+BASE=`dirname $(readlink -f $0)`
+
+die() {
+	echo "$*"
+	exit 1
+}
+
+wine --version >/dev/null 2>&1 || die "wine not found"
+DST=`wine winepath -u 'c:\windows\system32'`
+
+echo "installing 32bit binaries to $DST"
+install -m 644 "$BASE/lib32/d3d9-nine.dll.so" "$DST/d3d9-nine.dll"
+install -m 755 "$BASE/bin32/ninewinecfg.exe.so" "$DST/ninewinecfg.exe"
+
+unset HAVE_WINE64
+wine64 --version >/dev/null 2>&1 && HAVE_WINE64=1
+
+if test -z "$HAVE_WINE64"; then
+	echo "wine64 not found, skipping 64bit"
+	echo "enabling gallium nine"
+	wine ninewinecfg.exe -e
+	exit 0
+fi
+
+DST=`wine64 winepath -u 'c:\windows\system32'`
+
+echo "installing 64bit binaries to $DST"
+install -m 644 "$BASE/lib64/d3d9-nine.dll.so" "$DST/d3d9-nine.dll"
+install -m 755 "$BASE/bin64/ninewinecfg.exe.so" "$DST/ninewinecfg.exe"
+
+echo "enabling gallium nine"
+wine64 ninewinecfg.exe -e
+
+echo "done"
+exit 0
