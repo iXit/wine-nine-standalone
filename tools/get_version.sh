8f2f8002272305473a357f10e28626b913830d1e
Andre Heider <a.heider@gmail.com>
2019-01-15 08:43:26 +0100

    add 'stage' as version suffix and prepare for release

--- tools/get_version.sh
+++ tools/get_version.sh
@@ -4,7 +4,8 @@ MAJOR=0
 MINOR=1
 BUILD=0
 REVISION=`git rev-list --count HEAD 2>/dev/null || echo "0"`
+STAGE="release"
 
-echo -n "$MAJOR.$MINOR.$BUILD.$REVISION"
+echo -n "$MAJOR.$MINOR.$BUILD.$REVISION-$STAGE"
 
 exit 0
