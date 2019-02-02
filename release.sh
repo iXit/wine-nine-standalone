bb80d23898990c6c0357cd9f6513f2751a4ee607
Andre Heider <a.heider@gmail.com>
2019-02-02 10:24:30 +0100

    release.sh: add options to control the build
    -o FILE        create release as FILE
    -- MESONARGS   pass MESONARGS to meson

--- release.sh
+++ release.sh
@@ -2,6 +2,28 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 SRC=`dirname $(readlink -f $0)`
+OUT=$PWD/nine.tar.gz
+
+while getopts "o:h" opt; do
+	case "$opt" in
+		o)
+			OUT=$OPTARG
+			;;
+		h|\?)
+			printf "$0 [OPTION] [-- MESONARGS]\n"
+			printf "\t-o FILE\t\tcreate release as FILE\n"
+			printf "\t-h\t\tprint this help\n"
+			printf "\t-- MESONARGS\tpass MESONARGS to meson\n"
+			exit 1
+			;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+MESONARGS="$@"
+
+echo "creating $OUT"
+echo "additional meson args: $MESONARGS"
 
 $SRC/bootstrap.sh
 
@@ -13,6 +35,7 @@ meson \
 	--prefix "$TMP/nine" \
 	--bindir bin64 \
 	--libdir lib64 \
+	$MESONARGS \
 	"$TMP/build64"
 
 ninja -C "$TMP/build64" install
@@ -23,13 +46,14 @@ meson \
 	--prefix "$TMP/nine" \
 	--bindir bin32 \
 	--libdir lib32 \
+	$MESONARGS \
 	"$TMP/build32"
 
 ninja -C "$TMP/build32" install
 
 install -m 755 "$SRC/tools/nine-install.sh" "$TMP/nine/"
-tar -C "$TMP" -czf "$TMP/nine.tar.gz" nine
+tar -C "$TMP" -czf "$OUT" nine
 
-printf "\nenjoy your release: $TMP/nine.tar.gz\n"
+printf "\nenjoy your release: $OUT\n"
 
 exit 0
