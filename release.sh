20d1b204cf9e885a48cbd2a16566fa71703e8d63
Andre Heider <a.heider@gmail.com>
2019-02-02 14:15:37 +0100

    release.sh: use 'gallium-nine-standalone' as name

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 SRC=`dirname $(readlink -f $0)`
-OUT=$PWD/nine.tar.gz
+OUT=$PWD/gallium-nine-standalone.tar.gz
 
 while getopts "o:h" opt; do
 	case "$opt" in
@@ -28,11 +28,12 @@ echo "additional meson args: $MESONARGS"
 $SRC/bootstrap.sh
 
 TMP=`mktemp -d`
+PREFIX="$TMP/gallium-nine-standalone"
 
 meson \
 	--cross-file "$SRC/tools/cross-wine64" \
 	--buildtype "release" \
-	--prefix "$TMP/nine" \
+	--prefix "$PREFIX" \
 	--bindir bin64 \
 	--libdir lib64 \
 	$MESONARGS \
@@ -43,7 +44,7 @@ ninja -C "$TMP/build64" install
 meson \
 	--cross-file "$SRC/tools/cross-wine32" \
 	--buildtype "release" \
-	--prefix "$TMP/nine" \
+	--prefix "$PREFIX" \
 	--bindir bin32 \
 	--libdir lib32 \
 	$MESONARGS \
@@ -51,10 +52,10 @@ meson \
 
 ninja -C "$TMP/build32" install
 
-install -m 644 "$SRC/LICENSE" "$TMP/nine/"
-install -m 644 "$SRC/README.rst" "$TMP/nine/"
-install -m 755 "$SRC/tools/nine-install.sh" "$TMP/nine/"
-tar --owner=nine:1000 --group=nine:1000 -C "$TMP" -czf "$OUT" nine
+install -m 644 "$SRC/LICENSE" "$PREFIX/"
+install -m 644 "$SRC/README.rst" "$PREFIX/"
+install -m 755 "$SRC/tools/nine-install.sh" "$PREFIX/"
+tar --owner=nine:1000 --group=nine:1000 -C "$TMP" -czf "$OUT" gallium-nine-standalone
 
 printf "\nenjoy your release: $OUT\n"
 
