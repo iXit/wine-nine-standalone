iXit/wine-nine-standalone

commit 5eca1e64563240ab8b7c3bb3f6223c1b264bdfce

ninewinecfg: don't use BS_AUTOCHECKBOX for toggling nine
We set the state afterwards anyway.

ninewinecfg/main.c
@@ -854,7 +854,7 @@ static INT_PTR CALLBACK AppDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM l
         switch (LOWORD(wParam))
         {
         case IDC_ENABLE_NATIVE_D3D9:
-            nine_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_NATIVE_D3D9) == BST_CHECKED, FALSE);
+            nine_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_NATIVE_D3D9) == BST_UNCHECKED, TRUE);
             CheckDlgButton(hDlg, IDC_ENABLE_NATIVE_D3D9, nine_get() ? BST_CHECKED : BST_UNCHECKED);
             SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
             return TRUE;

ninewinecfg/ninewinecfg.rc
@@ -56,7 +56,7 @@ FONT 8, "MS Shell Dlg"
 BEGIN
     GROUPBOX    "Gallium Nine settings",IDC_STATIC,8,4,244,60
     LTEXT       "The following settings are experimental and may break stuff!\nMake sure to reset them again in case of a problem.\nGallium Nine requires MESA graphic drivers and AMD/Nvidia GPU.\n",IDC_STATIC,16,16,230,24
-    CONTROL     "Enable &Gallium Nine for better D3D9 graphic performance.",IDC_ENABLE_NATIVE_D3D9,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,16,44,230,8
+    CONTROL     "Enable &Gallium Nine for better D3D9 graphic performance.",IDC_ENABLE_NATIVE_D3D9,"Button",BS_CHECKBOX | WS_TABSTOP,16,44,230,8
 
     GROUPBOX    "Install state",IDC_STATIC,8,74,244,134
     CONTROL     "Support for Gallium Nine compiled in",IDC_NINE_STATE1,"Button",BS_AUTOCHECKBOX | WS_DISABLED,16,90,230,8

commit a60b98364636bbc7cd14efecd3785e651661fb85

ninewinecfg: fix toggling nine
Toggling nine in the 32bit GUI didn't create/remove the 64bit shortcut
(and vice versa).

Fixes: 5eca1e64 ("ninewinecfg: don't use BS_AUTOCHECKBOX for toggling nine")

ninewinecfg/main.c
@@ -642,7 +642,7 @@ static INT_PTR CALLBACK AppDlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM l
         switch (LOWORD(wParam))
         {
         case IDC_ENABLE_NATIVE_D3D9:
-            nine_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_NATIVE_D3D9) == BST_UNCHECKED, TRUE);
+            nine_set(IsDlgButtonChecked(hDlg, IDC_ENABLE_NATIVE_D3D9) == BST_UNCHECKED, FALSE);
             CheckDlgButton(hDlg, IDC_ENABLE_NATIVE_D3D9, nine_get() ? BST_CHECKED : BST_UNCHECKED);
             SendMessageW(GetParent(hDlg), PSM_CHANGED, 0, 0);
             return TRUE;