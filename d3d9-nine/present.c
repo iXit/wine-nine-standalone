siro20/wine

d3d9-nine: Return error if PRESENTinit fails
Signed-off-by: Patrick Rudolph <siro@das-labor.org>

dlls/d3d9-nine/present.c
@@ -1389,7 +1389,11 @@ static HRESULT DRI3Present_new(Display *gdi_display, const WCHAR *devname,
 
     strcpyW(This->devname, devname);
 
-    PRESENTInit(gdi_display, &(This->present_priv));
+    if (!PRESENTInit(gdi_display, &(This->present_priv)))
+    {
+        ERR("Failed to init Present backend\n");
+        return D3DERR_DRIVERINTERNALERROR;
+    }
 
     if (!DRIBackendInit(This->dri_backend, &(This->dri2_priv)))
     {

iXit/wine-nine-standalone

d3d9-nine/present.c
@@ -1424,7 +1424,11 @@ static HRESULT DRI3Present_new(Display *gdi_display, const WCHAR *devname,
 
     lstrcpyW(This->devname, devname);
 
-    PRESENTInit(gdi_display, &(This->present_priv));
+    if (!PRESENTInit(gdi_display, &(This->present_priv)))
+    {
+        WINE_ERR("Failed to init Present backend\n");
+        return D3DERR_DRIVERINTERNALERROR;
+    }
 
     if (!DRIBackendInit(This->dri_backend, &(This->dri2_priv)))
     {