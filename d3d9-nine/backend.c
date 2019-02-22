siro20/wine

d3d9-nine: Introduce DRIBackendPresentPixmap
Introduce PRESENTPixmapPrepare, DRI2PresentPixmap and
DRIBackendPresentPixmap, to abstract custom operations
required to update the pixmap.

Signed-off-by: Patrick Rudolph <siro@das-labor.org>

dlls/d3d9-nine/backend.c
@@ -210,3 +210,15 @@ void DRIBackendDestroy(struct DRIBackend *dri_backend, struct DRI2priv *dri2_pri
         DRI2FallbackDestroy(dri2_pri);
 #endif
 }
+
+void DRIBackendPresentPixmap(struct DRIBackend *dri_backend, struct DRI2priv *dri2_priv,
+        PRESENTPixmapPriv *present_priv)
+{
+    TRACE("dri_backend=%p dri2_priv=%p present_priv=%p\n", dri_backend, dri2_priv,
+        present_priv);
+
+#ifdef D3D9NINE_DRI2
+    if (dri_backend->type == TYPE_DRI2)
+        DRI2PresentPixmap(dri2_priv, present_priv);
+#endif
+}

dlls/d3d9-nine/dri3.c
@@ -1158,21 +1158,40 @@ BOOL PRESENTHelperCopyFront(Display *dpy, PRESENTPixmapPriv *present_pixmap_priv
     return (error != NULL);
 }
 
-BOOL PRESENTPixmap(Display *dpy, XID window, PRESENTPixmapPriv *present_pixmap_priv,
-        const UINT PresentationInterval, const BOOL PresentAsync, const BOOL SwapEffectCopy,
-        const RECT *pSourceRect, const RECT *pDestRect, const RGNDATA *pDirtyRegion)
+#ifdef D3D9NINE_DRI2
+BOOL DRI2PresentPixmap(struct DRI2priv *dri2_priv, PRESENTPixmapPriv *present_pixmap_priv)
+{
+    EGLenum current_api = 0;
+
+    current_api = eglQueryAPI();
+    eglBindAPI(EGL_OPENGL_API);
+    if (eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE,
+            EGL_NO_SURFACE, dri2_priv->context))
+    {
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_read);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_write);
+
+        glBlitFramebuffer(0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
+                0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
+                GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        glFlush(); /* Perhaps useless */
+    }
+    else
+    {
+        ERR("eglMakeCurrent failed with 0x%0X\n", eglGetError());
+        return FALSE;
+    }
+
+    eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglBindAPI(current_api);
+
+    return TRUE;
+}
+#endif
+
+BOOL PRESENTPixmapPrepare(XID window, PRESENTPixmapPriv *present_pixmap_priv)
 {
     PRESENTpriv *present_priv = present_pixmap_priv->present_priv;
-#ifdef D3D9NINE_DRI2
-    struct DRI2priv *dri2_priv = present_pixmap_priv->dri2_info.dri2_priv;
-    EGLenum current_api = 0;
-#endif
-    xcb_void_cookie_t cookie;
-    xcb_generic_error_t *error;
-    int64_t target_msc, presentationInterval;
-    xcb_xfixes_region_t valid, update;
-    int16_t x_off, y_off;
-    uint32_t options = XCB_PRESENT_OPTION_NONE;
 
     EnterCriticalSection(&present_priv->mutex_present);
 
@@ -1196,28 +1215,25 @@ BOOL PRESENTPixmap(Display *dpy, XID window, PRESENTPixmapPriv *present_pixmap_p
         LeaveCriticalSection(&present_priv->mutex_present);
         return FALSE;
     }
-#ifdef D3D9NINE_DRI2
-    if (present_pixmap_priv->dri2_info.is_dri2)
-    {
-        current_api = eglQueryAPI();
-        eglBindAPI(EGL_OPENGL_API);
-        if (eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, dri2_priv->context))
-        {
-            glBindFramebuffer(GL_READ_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_read);
-            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_write);
 
-            glBlitFramebuffer(0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
-                              0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
-                              GL_COLOR_BUFFER_BIT, GL_NEAREST);
-            glFlush(); /* Perhaps useless */
-        }
-        else
-            ERR("eglMakeCurrent failed with 0x%0X\n", eglGetError());
+    LeaveCriticalSection(&present_priv->mutex_present);
+    return TRUE;
+}
 
-        eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        eglBindAPI(current_api);
-    }
-#endif
+BOOL PRESENTPixmap(XID window, PRESENTPixmapPriv *present_pixmap_priv,
+        const UINT PresentationInterval, const BOOL PresentAsync, const BOOL SwapEffectCopy,
+        const RECT *pSourceRect, const RECT *pDestRect, const RGNDATA *pDirtyRegion)
+{
+    PRESENTpriv *present_priv = present_pixmap_priv->present_priv;
+    xcb_void_cookie_t cookie;
+    xcb_generic_error_t *error;
+    int64_t target_msc, presentationInterval;
+    xcb_xfixes_region_t valid, update;
+    int16_t x_off, y_off;
+    uint32_t options = XCB_PRESENT_OPTION_NONE;
+
+    EnterCriticalSection(&present_priv->mutex_present);
+
     target_msc = present_priv->last_msc;
 
     presentationInterval = PresentationInterval;

dlls/d3d9-nine/present.c
@@ -504,7 +504,17 @@ static HRESULT WINAPI DRI3Present_PresentBuffer( struct DRI3Present *This,
         }
     }
 
-    if (!PRESENTPixmap(This->gdi_display, d3d->drawable, buffer->present_pixmap_priv,
+    if (!PRESENTPixmapPrepare(d3d->drawable, buffer->present_pixmap_priv))
+    {
+        release_d3d_drawable(d3d);
+        ERR("PresentPrepare call failed\n");
+        return D3DERR_DRIVERINTERNALERROR;
+    }
+
+    /* FIMXE: Do we need to aquire present mutex here? */
+    DRIBackendPresentPixmap(This->dri_backend, This->dri2_priv, buffer->present_pixmap_priv);
+
+    if (!PRESENTPixmap(d3d->drawable, buffer->present_pixmap_priv,
             This->present_interval, This->present_async, This->present_swapeffectcopy,
             pSourceRect, pDestRect, pDirtyRegion))
     {

iXit/wine-nine-standalone

d3d9-nine/backend.c
@@ -192,3 +192,15 @@ void DRIBackendDestroy(struct DRIBackend *dri_backend, struct DRI2priv *dri2_pri
         DRI2FallbackDestroy(dri2_pri);
 #endif
 }
+
+void DRIBackendPresentPixmap(struct DRIBackend *dri_backend, struct DRI2priv *dri2_priv,
+        PRESENTPixmapPriv *present_priv)
+{
+    WINE_TRACE("dri_backend=%p dri2_priv=%p present_priv=%p\n", dri_backend, dri2_priv,
+        present_priv);
+
+#ifdef D3D9NINE_DRI2
+    if (dri_backend->type == TYPE_DRI2)
+        DRI2PresentPixmap(dri2_priv, present_priv);
+#endif
+}

d3d9-nine/dri3.c
@@ -1149,21 +1149,40 @@ BOOL PRESENTHelperCopyFront(Display *dpy, PRESENTPixmapPriv *present_pixmap_priv
     return (error != NULL);
 }
 
-BOOL PRESENTPixmap(Display *dpy, XID window, PRESENTPixmapPriv *present_pixmap_priv,
-        const UINT PresentationInterval, const BOOL PresentAsync, const BOOL SwapEffectCopy,
-        const RECT *pSourceRect, const RECT *pDestRect, const RGNDATA *pDirtyRegion)
+#ifdef D3D9NINE_DRI2
+BOOL DRI2PresentPixmap(struct DRI2priv *dri2_priv, PRESENTPixmapPriv *present_pixmap_priv)
+{
+    EGLenum current_api = 0;
+
+    current_api = eglQueryAPI();
+    eglBindAPI(EGL_OPENGL_API);
+    if (eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE,
+            EGL_NO_SURFACE, dri2_priv->context))
+    {
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_read);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_write);
+
+        glBlitFramebuffer(0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
+                0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
+                GL_COLOR_BUFFER_BIT, GL_NEAREST);
+        glFlush(); /* Perhaps useless */
+    }
+    else
+    {
+        WINE_ERR("eglMakeCurrent failed with 0x%0X\n", eglGetError());
+        return FALSE;
+    }
+
+    eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglBindAPI(current_api);
+
+    return TRUE;
+}
+#endif
+
+BOOL PRESENTPixmapPrepare(XID window, PRESENTPixmapPriv *present_pixmap_priv)
 {
     PRESENTpriv *present_priv = present_pixmap_priv->present_priv;
-#ifdef D3D9NINE_DRI2
-    struct DRI2priv *dri2_priv = present_pixmap_priv->dri2_info.dri2_priv;
-    EGLenum current_api = 0;
-#endif
-    xcb_void_cookie_t cookie;
-    xcb_generic_error_t *error;
-    int64_t target_msc, presentationInterval;
-    xcb_xfixes_region_t valid, update;
-    int16_t x_off, y_off;
-    uint32_t options = XCB_PRESENT_OPTION_NONE;
 
     EnterCriticalSection(&present_priv->mutex_present);
 
@@ -1187,28 +1206,25 @@ BOOL PRESENTPixmap(Display *dpy, XID window, PRESENTPixmapPriv *present_pixmap_p
         LeaveCriticalSection(&present_priv->mutex_present);
         return FALSE;
     }
-#ifdef D3D9NINE_DRI2
-    if (present_pixmap_priv->dri2_info.is_dri2)
-    {
-        current_api = eglQueryAPI();
-        eglBindAPI(EGL_OPENGL_API);
-        if (eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, dri2_priv->context))
-        {
-            glBindFramebuffer(GL_READ_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_read);
-            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, present_pixmap_priv->dri2_info.fbo_write);
 
-            glBlitFramebuffer(0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
-                              0, 0, present_pixmap_priv->width, present_pixmap_priv->height,
-                              GL_COLOR_BUFFER_BIT, GL_NEAREST);
-            glFlush(); /* Perhaps useless */
-        }
-        else
-            WINE_ERR("eglMakeCurrent failed with 0x%0X\n", eglGetError());
+    LeaveCriticalSection(&present_priv->mutex_present);
+    return TRUE;
+}
 
-        eglMakeCurrent(dri2_priv->display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-        eglBindAPI(current_api);
-    }
-#endif
+BOOL PRESENTPixmap(XID window, PRESENTPixmapPriv *present_pixmap_priv,
+        const UINT PresentationInterval, const BOOL PresentAsync, const BOOL SwapEffectCopy,
+        const RECT *pSourceRect, const RECT *pDestRect, const RGNDATA *pDirtyRegion)
+{
+    PRESENTpriv *present_priv = present_pixmap_priv->present_priv;
+    xcb_void_cookie_t cookie;
+    xcb_generic_error_t *error;
+    int64_t target_msc, presentationInterval;
+    xcb_xfixes_region_t valid, update;
+    int16_t x_off, y_off;
+    uint32_t options = XCB_PRESENT_OPTION_NONE;
+
+    EnterCriticalSection(&present_priv->mutex_present);
+
     target_msc = present_priv->last_msc;
 
     presentationInterval = PresentationInterval;

d3d9-nine/present.c
@@ -539,7 +539,17 @@ static HRESULT WINAPI DRI3Present_PresentBuffer( struct DRI3Present *This,
         }
     }
 
-    if (!PRESENTPixmap(This->gdi_display, d3d->drawable, buffer->present_pixmap_priv,
+    if (!PRESENTPixmapPrepare(d3d->drawable, buffer->present_pixmap_priv))
+    {
+        release_d3d_drawable(d3d);
+        WINE_ERR("PresentPrepare call failed\n");
+        return D3DERR_DRIVERINTERNALERROR;
+    }
+
+    /* FIMXE: Do we need to aquire present mutex here? */
+    DRIBackendPresentPixmap(This->dri_backend, This->dri2_priv, buffer->present_pixmap_priv);
+
+    if (!PRESENTPixmap(d3d->drawable, buffer->present_pixmap_priv,
             This->present_interval, This->present_async, This->present_swapeffectcopy,
             pSourceRect, pDestRect, pDirtyRegion))
     {