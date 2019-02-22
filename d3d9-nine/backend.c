siro20/wine

d3d9-nine: Introduce DRIBackendCheckExtension
Check both, support for DRI3 and DRI2 in backend.c.

Signed-off-by: Patrick Rudolph <siro@das-labor.org>

dlls/d3d9-nine/backend.c
@@ -90,3 +90,28 @@ void DRIBackendClose(struct DRIBackend *dri_backend)
         HeapFree(GetProcessHeap(), 0, dri_backend);
     }
 }
+
+BOOL DRIBackendCheckExtension(Display *dpy)
+{
+    TRACE("dpy=%p\n", dpy);
+
+    if (!dpy)
+        return FALSE;
+
+    if (!DRI3CheckExtension(dpy, 1, 0))
+    {
+#ifndef D3D9NINE_DRI2
+        ERR("Unable to query DRI3.\n");
+        return FALSE;
+#else
+        ERR("Unable to query DRI3. Trying DRI2 fallback (slower performance).\n");
+        is_dri2_fallback = 1;
+        if (!DRI2FallbackCheckSupport(dpy))
+        {
+            ERR("DRI2 fallback unsupported\n");
+            return FALSE;
+        }
+#endif
+    }
+    return TRUE;
+}

dlls/d3d9-nine/present.c
@@ -1783,20 +1783,10 @@ use_default_path:
         goto cleanup;
     }
 
-    if (!DRI3CheckExtension(gdi_display, 1, 0))
+    if (!DRIBackendCheckExtension(gdi_display))
     {
-#ifndef D3D9NINE_DRI2
-        ERR("Unable to query DRI3.\n");
+        ERR("Required extensions for DRIBackend not available.\n");
         goto cleanup;
-#else
-        ERR("Unable to query DRI3. Trying DRI2 fallback (slower performance).\n");
-        is_dri2_fallback = 1;
-        if (!DRI2FallbackCheckSupport(gdi_display))
-        {
-            ERR("DRI2 fallback unsupported\n");
-            goto cleanup;
-        }
-#endif
     }
 
     return TRUE;

iXit/wine-nine-standalone

d3d9-nine/backend.c
@@ -72,3 +72,28 @@ void DRIBackendClose(struct DRIBackend *dri_backend)
         HeapFree(GetProcessHeap(), 0, dri_backend);
     }
 }
+
+BOOL DRIBackendCheckExtension(Display *dpy)
+{
+    WINE_TRACE("dpy=%p\n", dpy);
+
+    if (!dpy)
+        return FALSE;
+
+    if (!DRI3CheckExtension(dpy, 1, 0))
+    {
+#ifndef D3D9NINE_DRI2
+        WINE_ERR("Unable to query DRI3.\n");
+        return FALSE;
+#else
+        WINE_ERR("Unable to query DRI3. Trying DRI2 fallback (slower performance).\n");
+        is_dri2_fallback = 1;
+        if (!DRI2FallbackCheckSupport(dpy))
+        {
+            WINE_ERR("DRI2 fallback unsupported\n");
+            return FALSE;
+        }
+#endif
+    }
+    return TRUE;
+}

d3d9-nine/present.c
@@ -1712,20 +1712,10 @@ BOOL present_has_d3dadapter(Display *gdi_display)
         goto cleanup;
     }
 
-    if (!DRI3CheckExtension(gdi_display, 1, 0))
+    if (!DRIBackendCheckExtension(gdi_display))
     {
-#ifndef D3D9NINE_DRI2
-        WINE_ERR("Unable to query DRI3.\n");
+        WINE_ERR("Required extensions for DRIBackend not available.\n");
         goto cleanup;
-#else
-        WINE_ERR("Unable to query DRI3. Trying DRI2 fallback (slower performance).\n");
-        is_dri2_fallback = 1;
-        if (!DRI2FallbackCheckSupport(gdi_display))
-        {
-            WINE_ERR("DRI2 fallback unsupported\n");
-            goto cleanup;
-        }
-#endif
     }
 
     return TRUE;