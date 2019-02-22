siro20/wine

d3d9-nine: Introduce DRIBackend* functions
Abstract the DRIBackend by a new interface.
Migrate DRI*Open to this new API.

Should not change any functionality.

Signed-off-by: Patrick Rudolph <siro@das-labor.org>

dlls/d3d9-nine/d3dadapter9.c
@@ -31,6 +31,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 #include <d3dadapter/d3dadapter9.h>
 #include "present.h"
 #include "device_wrap.h"
+#include "backend.h"
 
 /* this represents a snapshot taken at the moment of creation */
 struct output
@@ -56,6 +57,8 @@ struct adapter_group
 
     /* driver stuff */
     ID3DAdapter9 *adapter;
+    /* DRI backend */
+    struct DRIBackend *dri_backend;
 };
 
 struct adapter_map
@@ -136,6 +139,8 @@ static ULONG WINAPI d3dadapter9_Release(struct d3dadapter9 *This)
 
                 if (This->groups[i].adapter)
                     ID3DAdapter9_Release(This->groups[i].adapter);
+                if (This->groups[i].dri_backend)
+                    DRIBackendClose(This->groups[i].dri_backend);
             }
             HeapFree(GetProcessHeap(), 0, This->groups);
         }
@@ -572,6 +577,7 @@ static void remove_group(struct d3dadapter9 *This)
         HeapFree(GetProcessHeap(), 0, group->outputs[i].modes);
     }
     HeapFree(GetProcessHeap(), 0, group->outputs);
+    DRIBackendClose(group->dri_backend);
 
     ZeroMemory(group, sizeof(struct adapter_group));
     This->ngroups--;
@@ -687,7 +693,16 @@ static HRESULT fill_groups(struct d3dadapter9 *This)
             goto end_group;
         }
 
-        hr = present_create_adapter9(This->gdi_display, hdc, &group->adapter);
+        if (!DRIBackendOpen(This->gdi_display, DefaultScreen(This->gdi_display),
+                &group->dri_backend))
+        {
+            ERR("Unable to open DRIBackend for display %d.\n", i);
+            goto end_group;
+        }
+
+        hr = present_create_adapter9(This->gdi_display, hdc, group->dri_backend,
+               &group->adapter);
+
         DeleteDC(hdc);
         if (FAILED(hr))
         {

dlls/d3d9-nine/present.c
@@ -36,6 +36,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 
 #include "dri3.h"
 #include "wndproc.h"
+#include "backend.h"
 
 #include "wine/library.h" // for wine_dl*
 #include "wine/unicode.h" // for strcpyW
@@ -57,7 +58,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 
 static const struct D3DAdapter9DRM *d3d9_drm = NULL;
 #ifdef D3D9NINE_DRI2
-static int is_dri2_fallback = 0;
+int is_dri2_fallback = 0;
 #endif
 
 /* Start section of x11drv.h */
@@ -1593,7 +1594,8 @@ HRESULT present_create_present_group(Display *gdi_display, const WCHAR *device_n
     return D3D_OK;
 }
 
-HRESULT present_create_adapter9(Display *gdi_display, HDC hdc, ID3DAdapter9 **out)
+HRESULT present_create_adapter9(Display *gdi_display, HDC hdc,
+        struct DRIBackend *dri_backend, ID3DAdapter9 **out)
 {
     struct x11drv_escape_get_drawable extesc = { X11DRV_GET_DRAWABLE };
     HRESULT hr;
@@ -1609,22 +1611,12 @@ HRESULT present_create_adapter9(Display *gdi_display, HDC hdc, ID3DAdapter9 **ou
             sizeof(extesc), (LPSTR)&extesc) <= 0)
         ERR("X11 drawable lookup failed (hdc=%p)\n", hdc);
 
-#ifdef D3D9NINE_DRI2
-    if (!is_dri2_fallback && !DRI3Open(gdi_display, DefaultScreen(gdi_display), &fd))
-#else
-    if (!DRI3Open(gdi_display, DefaultScreen(gdi_display), &fd))
-#endif
-    {
-        ERR("DRI3Open failed (fd=%d)\n", fd);
+    fd = DRIBackendFd(dri_backend);
+    if (fd < 0) {
+        ERR("Got invalid fd from DRIBackend (fd=%d)\n", fd);
         return D3DERR_DRIVERINTERNALERROR;
     }
-#ifdef D3D9NINE_DRI2
-    if (is_dri2_fallback && !DRI2FallbackOpen(gdi_display, DefaultScreen(gdi_display), &fd))
-    {
-        ERR("DRI2Open failed (fd=%d)\n", fd);
-        return D3DERR_DRIVERINTERNALERROR;
-    }
-#endif
+
     hr = d3d9_drm->create_adapter(fd, out);
     if (FAILED(hr))
     {

iXit/wine-nine-standalone

d3d9-nine/d3dadapter9.c
@@ -15,6 +15,7 @@
 
 #include "present.h"
 #include "device_wrap.h"
+#include "backend.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 
@@ -42,6 +43,8 @@ struct adapter_group
 
     /* driver stuff */
     ID3DAdapter9 *adapter;
+    /* DRI backend */
+    struct DRIBackend *dri_backend;
 };
 
 struct adapter_map
@@ -122,6 +125,8 @@ static ULONG WINAPI d3dadapter9_Release(struct d3dadapter9 *This)
 
                 if (This->groups[i].adapter)
                     ID3DAdapter9_Release(This->groups[i].adapter);
+                if (This->groups[i].dri_backend)
+                    DRIBackendClose(This->groups[i].dri_backend);
             }
             HeapFree(GetProcessHeap(), 0, This->groups);
         }
@@ -558,6 +563,7 @@ static void remove_group(struct d3dadapter9 *This)
         HeapFree(GetProcessHeap(), 0, group->outputs[i].modes);
     }
     HeapFree(GetProcessHeap(), 0, group->outputs);
+    DRIBackendClose(group->dri_backend);
 
     ZeroMemory(group, sizeof(struct adapter_group));
     This->ngroups--;
@@ -673,7 +679,16 @@ static HRESULT fill_groups(struct d3dadapter9 *This)
             goto end_group;
         }
 
-        hr = present_create_adapter9(This->gdi_display, hdc, &group->adapter);
+        if (!DRIBackendOpen(This->gdi_display, DefaultScreen(This->gdi_display),
+                &group->dri_backend))
+        {
+            WINE_ERR("Unable to open DRIBackend for display %d.\n", i);
+            goto end_group;
+        }
+
+        hr = present_create_adapter9(This->gdi_display, hdc, group->dri_backend,
+               &group->adapter);
+
         DeleteDC(hdc);
         if (FAILED(hr))
         {

d3d9-nine/present.c
@@ -20,6 +20,7 @@
 #include <dlfcn.h>
 
 #include "../common/library.h"
+#include "backend.h"
 #include "dri3.h"
 #include "wndproc.h"
 
@@ -42,7 +43,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 
 static const struct D3DAdapter9DRM *d3d9_drm = NULL;
 #ifdef D3D9NINE_DRI2
-static int is_dri2_fallback = 0;
+int is_dri2_fallback = 0;
 #endif
 
 /* Start section of x11drv.h */
@@ -1628,7 +1629,8 @@ HRESULT present_create_present_group(Display *gdi_display, const WCHAR *device_n
     return D3D_OK;
 }
 
-HRESULT present_create_adapter9(Display *gdi_display, HDC hdc, ID3DAdapter9 **out)
+HRESULT present_create_adapter9(Display *gdi_display, HDC hdc,
+        struct DRIBackend *dri_backend, ID3DAdapter9 **out)
 {
     HRESULT hr;
     int fd;
@@ -1642,22 +1644,12 @@ HRESULT present_create_adapter9(Display *gdi_display, HDC hdc, ID3DAdapter9 **ou
     if (!get_wine_drawable_from_dc(hdc, NULL))
         return D3DERR_DRIVERINTERNALERROR;
 
-#ifdef D3D9NINE_DRI2
-    if (!is_dri2_fallback && !DRI3Open(gdi_display, DefaultScreen(gdi_display), &fd))
-#else
-    if (!DRI3Open(gdi_display, DefaultScreen(gdi_display), &fd))
-#endif
-    {
-        WINE_ERR("DRI3Open failed (fd=%d)\n", fd);
+    fd = DRIBackendFd(dri_backend);
+    if (fd < 0) {
+        WINE_ERR("Got invalid fd from DRIBackend (fd=%d)\n", fd);
         return D3DERR_DRIVERINTERNALERROR;
     }
-#ifdef D3D9NINE_DRI2
-    if (is_dri2_fallback && !DRI2FallbackOpen(gdi_display, DefaultScreen(gdi_display), &fd))
-    {
-        WINE_ERR("DRI2Open failed (fd=%d)\n", fd);
-        return D3DERR_DRIVERINTERNALERROR;
-    }
-#endif
+
     hr = d3d9_drm->create_adapter(fd, out);
     if (FAILED(hr))
     {