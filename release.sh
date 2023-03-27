ef971dd4ce087e003d0aa54702a881cd8ad6e082
novenary <streetwalkermc@gmail.com>
2023-03-27 11:04:10 +0300

    Fix winetricks compatibility
    winetricks can download future releases that it doesn't package yet.
    
    Because it doesn't use our install script, we need to preserve the
    existing structure in our release archive.
    
    Let's just link the files back to the right place until downstreams
    catch up.

--- release.sh
+++ release.sh
@@ -49,6 +49,17 @@ meson \
 
 ninja -C "$TMP/build32" install
 
+# winetricks backwards compatibility
+# Hard links should be safe here, and avoid bloating up the archive size.
+install -d "$PREFIX/lib64"
+ln "$PREFIX/wine/x86_64-unix/d3d9-nine.dll.so" "$PREFIX/lib64/d3d9-nine.dll.so"
+install -d "$PREFIX/bin64"
+ln "$PREFIX/wine/x86_64-unix/ninewinecfg.exe.so" "$PREFIX/bin64/ninewinecfg.exe.so"
+install -d "$PREFIX/lib32"
+ln "$PREFIX/wine/i386-unix/d3d9-nine.dll.so" "$PREFIX/lib32/d3d9-nine.dll.so"
+install -d "$PREFIX/bin32"
+ln "$PREFIX/wine/i386-unix/ninewinecfg.exe.so" "$PREFIX/bin32/ninewinecfg.exe.so"
+
 install -m 644 "$SRC/LICENSE" "$PREFIX/"
 install -m 644 "$SRC/README.rst" "$PREFIX/"
 install -m 755 "$SRC/tools/nine-install.sh" "$PREFIX/"
